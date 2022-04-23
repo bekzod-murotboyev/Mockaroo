5c05418a04cd305d51cebffebe16fcab97092a35
Botirbek <botirbek18022000@mail.ru>
2022-04-23 13:45:14 +0500

    4 -commit

--- src/main/java/uz/pdp/mockaroo/controller/MockController.java
+++ src/main/java/uz/pdp/mockaroo/controller/MockController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uz.pdp.mockaroo.payload.request.ApiRequestSql;
+import uz.pdp.mockaroo.payload.request.base.ApiRequest;
 import uz.pdp.mockaroo.payload.response.ApiResponse;
 import uz.pdp.mockaroo.service.MockingService;
+import uz.pdp.mockaroo.util.Data;
 
 @RestController
 @RequestMapping("/mock")
@@ -15,15 +17,15 @@ public record MockController(MockingService mockingService){
 
     // IT IS ONLY FOR SQL, FOR USING OTHER FORMAT YOU NEED TO CREATE ANOTHER METHOD WITH ANOTHER URL
     @PostMapping("/download")
-    public ResponseEntity<?> download(@RequestBody ApiRequestSql request) {
+    public ResponseEntity<Data> download(@RequestBody ApiRequest request) {
         ApiResponse response=mockingService.download(request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(new Data(response));
     }
 
     @PostMapping("/check")
-    public ResponseEntity<?> check(@RequestBody ApiRequestSql request) {
+    public ResponseEntity<Data> check(@RequestBody ApiRequest request) {
         ApiResponse response=mockingService.check(request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(new Data(response));
     }
 
 
